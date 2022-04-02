52513c6c840151a341be657f0acd536df8ec8852
Wojciech Czepukojć <wojciechczepukojc@gmail.com>
2022-04-02 20:15:22 +0200

    Bledy

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -1,2 +1,16 @@
 public class Kalkulator {
+    {
+       int x = 25869;
+       int y = 3;
+       int z = x%y;{
+           if (z == 0)
+               System.out.print("Liczba"+x+"jest podzielna przez"+y);
+           else
+               System.out.print("Liczba"+x+"nie jest podzielna przez"+y);
+        }
+
+
+
+
+    }
 }
